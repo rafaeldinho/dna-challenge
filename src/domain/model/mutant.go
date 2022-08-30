c61f61bba40ea26da964d4b6d77140251bf79a00
Rafael Añez <rafael.az1302@gmail.com>
2022-08-30 15:23:05 -0400

    add: mongo client and deleting firetore for better use

--- src/domain/model/mutant.go
+++ src/domain/model/mutant.go
@@ -1,6 +1,6 @@
 package model
 
 type Mutant struct {
-	DNA      string `firestore:"dna,omitempty"`
-	IsMutant bool   `firestore:"isMutant,omitempty"`
+	DNA      string `json:"dna" bson:"dna"`
+	IsMutant bool   `json:"isMutant" bson:"isMutant"` 
 }
